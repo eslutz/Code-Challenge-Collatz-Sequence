8b78460b4a785063f4e27c775d79b0e8b14abcf5
Eric Slutz <eric.slutz@icloud.com>
2022-04-21 22:35:36 -0400

    added readme and comments

--- CollatzSequence/Program.cs
+++ CollatzSequence/Program.cs
@@ -1,38 +1,56 @@
-﻿namespace CollatzSequence
+﻿namespace CollatzSequence;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        // Prompt the user for input.
+        Console.Write("Enter a positive integer => ");
+        var userInput = Console.ReadLine();
+        // Attempt to parse the input as an unsigned long value.
+        var vaildNumberInput = ulong.TryParse(userInput, out ulong userNumber);
+
+        // If the user didn't input a valid number, enter loop.
+        while (!vaildNumberInput)
         {
+            // Prompt user for input again.
+            Console.WriteLine("Not a valid positive integer. Try again.");
             Console.Write("Enter a positive integer => ");
-            var userInput = Console.ReadLine();
-            var vaildNumberInput = ulong.TryParse(userInput, out ulong userNumber);
-
-            while (!vaildNumberInput)
-            {
-                Console.WriteLine("Not a valid positive integer. Try again.");
-                Console.Write("Enter a positive integer => ");
-                userInput = Console.ReadLine();
-                vaildNumberInput = ulong.TryParse(userInput, out userNumber);
-            }
-
-            var collatzSequence = GetCollatzSequence(userNumber);
-
-            collatzSequence.ForEach(Console.WriteLine);
+            userInput = Console.ReadLine();
+            // Attempt to parse the input as an unsigned long value.
+            vaildNumberInput = ulong.TryParse(userInput, out userNumber);
         }
 
-        static List<ulong> GetCollatzSequence(ulong number)
+        // Call collatz method, passing in the users number.
+        // Assign returned value.
+        var collatzSequence = GetCollatzSequence(userNumber);
+
+        // Print out the Collatz Sequence.
+        collatzSequence.ForEach(Console.WriteLine);
+    }
+
+    /// <summary>
+    /// Determines the Collatz Sequence for a number.
+    /// </summary>
+    /// <param name="number">Positive integer starting point.</param>
+    /// <returns>The Collatz Sequence.</returns>
+    static List<ulong> GetCollatzSequence(ulong number)
+    {
+        // Initializing Collatz Sequence list with starting number.
+        var collatzSequence = new List<ulong>() { number };
+
+        // Loop until the sequence reaches the end value of one.
+        do
         {
-            var collatzSequence = new List<ulong>() { number };
-
-            do
-            {
-                number = number % 2 == 0 ? number / 2 : 3 * number + 1;
-                collatzSequence.Add(number);
-            }
-            while (number > 1);
-
-            return collatzSequence;
+            // If even, divide the number by 2.
+            // If odd, multiply the number by 3, add 1, and divide by 2.
+            number = number % 2 == 0 ? number / 2 : (3 * number + 1) / 2;
+            // Add the new number to the sequence list.
+            collatzSequence.Add(number);
         }
+        while (number > 1);
+
+        // Return the completed Collatz Sequence.
+        return collatzSequence;
     }
 }
---
+++ docs/README.md
@@ -0,0 +1,7 @@
+﻿# Collatz Sequence
+
+This is a small console program to demostrate the Collatz Conjecture.
+
+> The Collatz conjecture in mathematics asks whether repeating two simple arithmetic operations will eventually transform every positive integer into one. It concerns sequences of integers in which each term is obtained from the previous term as follows: if the previous term is even, the next term is one half of the previous term. If the previous term is odd, the next term is 3 times the previous term plus 1. The conjecture is that these sequences always reach 1, no matter which positive integer is chosen to start the sequence.
+
+![Collatz Function](./collatz-function.svg)
