d54793abbc39ac2bd045c02788f6fb32ac66bfb1
Eric Slutz <eric.slutz@icloud.com>
2022-04-22 22:45:34 -0400

    fixed quit logic

--- CollatzSequence/Program.cs
+++ CollatzSequence/Program.cs
@@ -6,29 +6,42 @@ internal class Program
 {
     static void Main(string[] args)
     {
+        // Display welcome message.
 		Console.WriteLine("Welcome to the Collatz Conjecture Sequence Generator!");
+
+        // Initialize variable for keeping track of if the program is running.
         var quitProgram = false;
         while (!quitProgram)
         {
             // Prompt the user for input.
             Console.Write("\nEnter a positive integer (enter q to quit) => ");
             var userInput = Console.ReadLine();
+
             // Check if user wants to quit the program.
             quitProgram = new[] { "q", "quit" }.Contains(userInput, StringComparer.InvariantCultureIgnoreCase);
             if (quitProgram)
             {
                 continue;
             }
+
             // Attempt to parse the input as an unsigned long value.
             var vaildNumberInput = BigInteger.TryParse(userInput, out BigInteger userNumber);
 
             // If the user didn't input a valid number, enter loop.
-            while (!vaildNumberInput || userNumber <= 0)
+            while ((!vaildNumberInput || userNumber <= 0) && !quitProgram)
             {
                 // Prompt user for input again.
                 Console.WriteLine("\nNot a valid positive integer. Try again.");
                 Console.Write("Enter a positive integer (enter q to quit) => ");
                 userInput = Console.ReadLine();
+
+                // Check if user wants to quit the program.
+                quitProgram = new[] { "q", "quit" }.Contains(userInput, StringComparer.InvariantCultureIgnoreCase);
+                if (quitProgram)
+                {
+                    continue;
+                }
+
                 // Attempt to parse the input as an unsigned long value.
                 vaildNumberInput = BigInteger.TryParse(userInput, out userNumber);
             }
